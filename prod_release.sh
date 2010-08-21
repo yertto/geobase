c04ec6c4f5046535c8c72dbc9c6b447d32a98859
yertto <yertto@gmail.com>
2010-08-21 21:10:57 +1000

    updated prod_release.sh

--- prod_release.sh
+++ prod_release.sh
@@ -7,7 +7,7 @@ version=`cat VERSION`                            &&
 git commit -am "v$version release"               &&
 git tag v`cat VERSION`                           &&
 git push github                                  &&
-git push --tags                                  &&
+git push --tags github                           &&
 git push production                              &&
 printf "Now, update VERSION to ($version+1).rc1" &&
 read x                                           &&
